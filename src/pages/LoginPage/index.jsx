8535ae3766549f6d24aac6cac4b9371adf7f45f7
Bohdan <theodoronos@gmail.com>
2023-01-29 19:46:27 +0100

    Add structure of page and form

---
+++ src/pages/LoginPage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FormikForm from '../../components/forms/FormikForm'
+
+function LoginPage () {
+  return (
+    <section>
+      <h1>CREATE AN ACCOUNT</h1>
+      <h2>We always keep your name and email address private.</h2>
+      <FormikForm />
+    </section>
+  )
+}
+
+export default LoginPage
---
+++ src/utils/validate/validationSchemas.js
@@ -0,0 +1,36 @@
+import * as yup from 'yup'
+
+export const FORM_VALIDATION_SCHEMA = yup.object({
+  firstName: yup
+    .string()
+    .trim()
+    .min(2)
+    .max(64)
+    .required('Field cannot be empty'),
+  lastName: yup
+    .string()
+    .trim()
+    .min(2)
+    .max(64)
+    .required('Field cannot be empty'),
+  displayName: yup
+    .string()
+    .trim()
+    .min(4, 'Display name should be more than 4 characters')
+    .max(20)
+    .required(),
+  email: yup.string().email('Please check the format of email address')
+  // password: yup
+  //   .min(8)
+  //   .max(20)
+  //   .matches(/^(?=.*[A-Z].*)(?=.*[a-z].*)(?=.*\d.*)(?=.*[!@#$%^&*_].*).{8,32}$/)
+  //   .required(),
+  // passwordConfirmation: yup
+  //   .min(8)
+  //   .max(20)
+  //   .matches(
+  //     /^(?=.*[A-Z].*)(?=.*[a-z].*)(?=.*\d.*)(?=.*[!@#$%^&*_].*).{8,32}$/,
+  //     'Password confirmation needs to match original password'
+  //   )
+  //   .required()
+})
